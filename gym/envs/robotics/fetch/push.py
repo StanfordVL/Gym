01e9d2ba16e3dd5ba264243f4000c5c22646bdc8
charleslin4 <charleslin4@gmail.com>
2020-10-30 14:37:42 -0700

    contact reward for push task

--- gym/envs/robotics/fetch/push.py
+++ gym/envs/robotics/fetch/push.py
@@ -1,11 +1,14 @@
 import os
 from .... import utils
 from .. import fetch_env
-
+import numpy as np
 
 # Ensure we get the path separator correct on windows
 MODEL_XML_PATH = os.path.join('fetch', 'push.xml')
 
+def goal_distance(goal_a, goal_b):
+    assert goal_a.shape == goal_b.shape
+    return np.linalg.norm(goal_a - goal_b)
 
 class FetchPushEnv(fetch_env.FetchEnv, utils.EzPickle):
     def __init__(self, reward_type):
@@ -21,3 +24,26 @@ class FetchPushEnv(fetch_env.FetchEnv, utils.EzPickle):
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type)
         utils.EzPickle.__init__(self)
+
+        # TODO move magic numbers
+        self.contact_reward = 1.
+        self.contact_shaped_reward = 0.5
+
+    def compute_reward(self, achieved_goal, goal, info):
+        # Compute distance between goal and the achieved goal.
+        d = goal_distance(achieved_goal, goal)
+        if self.reward_type == 'sparse':
+            return -(d > self.distance_threshold).astype(np.float32)
+        elif self.reward_type == 'dense':
+            reward = 0
+            if self.has_object:
+                eef_position = self.sim.data.get_site_xpos('robot0:grip')
+                obj_position = self.sim.data.get_site_xpos('object0')
+                obj_dist = np.linalg.norm(eef_position - obj_position)
+                if obj_dist < self.distance_threshold and abs(eef_position[2] - obj_position[2]) < 0.02:
+                    reward += self.contact_reward
+                else:
+                    reward += (self.contact_shaped_reward * (1 - np.tanh(3 * obj_dist))).squeeze()
+            return reward - d
+        else:
+            return -d
--- gym/envs/robotics/fetch_env.py
+++ gym/envs/robotics/fetch_env.py
@@ -7,7 +7,6 @@ def goal_distance(goal_a, goal_b):
     assert goal_a.shape == goal_b.shape
     return np.linalg.norm(goal_a - goal_b) #, axis=-1)
 
-
 class FetchEnv(robot_env.RobotEnv):
     """Superclass for all Fetch environments.
     """
